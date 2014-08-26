f76b2b041891192e7e286d2a51d50de74ea49b47
dacin21 <daniel.rutschmann@stud.ksimlee.ch>
2014-08-26 10:06:20 +0200

    Display

--- Projects/src/main/Main.java
+++ Projects/src/main/Main.java
@@ -1,11 +1,45 @@
 package main;
 
+import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 
-public class Main {
+public class Main implements Runnable{
+	
+	private int width=1280;
+	private int height = 720;
+	private String title = "Test";
+	
+	private boolean running = false;
+	private Thread thread;
+	
+	public void start(){
+		running = true;
+		thread =  new Thread(this, "Display");
+		thread.start();
+	}
+	
+	public void run(){
+		try {
+			Display.setDisplayMode(new DisplayMode(width, height));
+			Display.setTitle(title);
+			Display.create();
+		} catch (LWJGLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		while(running){
+			
+			Display.update();
+			if(Display.isCloseRequested()) running = false;
+			
+		}
+		Display.destroy();
+	}
 
 	public static void main(String[] args) {
-		System.out.println("D scheiss bringts meh durenand, as es hilft");
+		new Main().start();
 
 	}
 	
