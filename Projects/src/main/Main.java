b29747384cd64372dc67f679435567e26af7f812
spyspyyou <sandro.rueegge@hispeed.ch>
2014-08-26 09:45:49 +0200

    nachricht

--- Projects/src/main/Main.java
+++ Projects/src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("De scheiss bringts meh durenand, as es hilft");
 
 	}
 
